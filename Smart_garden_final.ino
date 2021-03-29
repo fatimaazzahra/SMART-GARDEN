a0dffc2e6a4c327aac70018f36fea508642395a9
alectivski <57226819+alectivski@users.noreply.github.com>
2021-03-29 09:23:42 +0700

    Update Smart_garden_final.ino

--- Smart_garden_final.ino
+++ Smart_garden_final.ino
@@ -1,5 +1,14 @@
 #include <LiquidCrystal_I2C.h>
+#include <AntaresESP8266MQTT.h>
+#define ACCESSKEY "ca55fee49ab1a048:ac85441edd41a2b7"
+#define WIFISSID "LIMBONG01"         
+#define PASSWORD "nelaangela"     
+#define projectName "SmartGarden01"
+#define deviceName "smartgarden"
+
+AntaresESP8266MQTT antares(ACCESSKEY);
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27 ,16, 2);
+
 int kelembaban = (A0);
 int cahaya = (D3);
 int pump = (D4);
@@ -13,18 +22,21 @@ void setup() {
   lcd.backlight();
   pinMode(D4, OUTPUT);
   pinMode(D5, OUTPUT);
+  antares.setDebug(true);
+  antares.wifiConnection(WIFISSID, PASSWORD);
+  antares.setMqttServer();
   
-  
-
-}
+  }
 
 void loop() {
+  antares.checkMqttConnection();
   int a = (analogRead(kelembaban));
+
   Serial.print("Sensor Cahaya Sebesar :");
   Serial.println(digitalRead(cahaya));
   Serial.print("Sensor Kelembapan Sebesar :");
-  Serial.print(persen);
-  Serial.println("%");
+  Serial.println(a);
+  
 
   if (digitalRead(cahaya)== 0) {
      lcd.setCursor(0,0);
@@ -39,18 +51,43 @@ void loop() {
      lcd.setCursor(0,0);
      lcd.print("Kurang Cahaya");
      delay(1000);
-     digitalWrite(lampu,HIGH);
+     digitalWrite(lampu,HIGH); }
 
-  if ((persen)< 50 )   {
+  if ((a)> 600 )   {
      lcd.setCursor(0,1);
-     lcd.print("Kelembaban : ");
-     lcd.print(persen);
-     lcd.print("%");
-     digitalWrite(pump,LOW);
+     lcd.print("Kelembaban: ");
+     lcd.print(a);
+     delay(1000);
+     digitalWrite(pump,HIGH);
      
      
      
   }
+  else {
+     lcd.setCursor(0,1);
+     lcd.print("Kelembaban: ");
+     lcd.print(a);
+
+     lcd.print("Kelembaban : ");
+     lcd.print(a);
+     lcd.print("%");
+
+     delay(1000);
+     digitalWrite(pump,LOW);
+     
+
+      
+    
+  }
+
+    
+
+  antares.add("Cahaya", cahaya);
+  antares.add("Kelembaban", a);  
+  antares.publish(projectName, deviceName);
+  delay(5000);
+
+  
   else {
      lcd.setCursor(0,1);
      lcd.print("Kelembaban : ");
